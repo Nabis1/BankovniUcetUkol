6c0ce4b1c323026864f4f4dddd27a37eb71f5cc5
Nabis <r.cernil332@gmail.com>
2023-12-15 15:05:59 +0100

    Add project files.

---
+++ BankovniUcetUkol/BankovniUcet.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+
+namespace BankovniUcetUkol
+{
+    public class BankovniUcet
+    {
+        public BankovniUcet(string jmeno, string prijmeni, double zustatek)
+        {
+            Jmeno = jmeno;
+            Prijmeni = prijmeni;
+            Zustatek = zustatek;
+
+        }
+
+        public string Jmeno = "David";
+        public string Prijmeni = "Pasta";
+        public double Zustatek = 0;
+
+
+        public void Identifikace()
+        {
+            Console.WriteLine(" Identifikace klienta ");
+            Console.WriteLine($" Jmeno a Prijmeni : {Jmeno}  {Prijmeni}");
+            Console.WriteLine($" Aktualni zustatek cini: {Zustatek} ");
+        }
+        public double VlozPenize()
+        {
+            Console.WriteLine("Kolik si prejete vlozit penez?");
+            double vlozitpenize = Convert.ToDouble(Console.ReadLine());
+            double novyzustatek = vlozitpenize + Zustatek;
+            Console.WriteLine($"Vas aktualni zustatek po vlozeni {vlozitpenize} kc je {novyzustatek} kc");
+            Console.WriteLine("-------------------------------");
+            return novyzustatek;
+        }
+        public double VyberPenize()
+        {
+            Console.WriteLine("Kolik si prejete vybrat penez?");
+            double vybratpenize = Convert.ToDouble(Console.ReadLine());
+            double novyzustatek = Zustatek - vybratpenize;
+            Console.WriteLine($"Vas aktualni zustatek po vybrani {vybratpenize} kc je {novyzustatek} kc");
+            Console.WriteLine("-------------------------------");
+            return novyzustatek;
+        }
+        public double MuzuVybratCastku()
+        {
+
+            Console.WriteLine("Zkuste vybrat libovolnou castku");
+            double muzuVybratCastku = Convert.ToDouble(Console.ReadLine());
+            if (Zustatek < muzuVybratCastku)
+            {
+                Console.WriteLine("Tuto Castku nemuzete vybrat, nemate dostatek financi");
+
+            }
+            else if (Zustatek > muzuVybratCastku)
+            {
+                Console.WriteLine("Tuto castku muzete vybrat");
+            }
+            return muzuVybratCastku;
+        }
+        public void VratZustatek()
+        {
+            Console.WriteLine($"Vratili jsme vam puvodni zustatek kterz jste zadal na zacatku ktery cini {Zustatek}kc");
+        }
+    }
+}
---
+++ BankovniUcetUkol/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BankovniUcetUkol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte pocatecni castku na vasem uctu");
+            BankovniUcet david = new BankovniUcet("David","Pasta", Convert.ToDouble(Console.ReadLine()));
+            david.Identifikace();
+            david.VlozPenize();
+            david.VyberPenize();
+            david.MuzuVybratCastku();
+            david.VratZustatek();
+        }
+    }
+}
